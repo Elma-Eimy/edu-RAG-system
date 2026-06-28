82f2a136cf31cee345230b3a960bec2d0a58ce96
Elma-Eimy <2075274894@qq.com>
2026-06-28 21:44:40 +0800

    Add files via upload

--- app-backend/crud/crud_user.py
+++ app-backend/crud/crud_user.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 from crud.base import CRUDBase
 from db.models.user import User
 
@@ -8,7 +8,7 @@ class CRUDUser(CRUDBase[User, Any, Any]):
     async def get_by_username(self, db: AsyncSession, username: str) -> Optional[User]:
         result = await db.execute(
             select(self.model).where(
-                self.model.username == username,
+                func.lower(self.model.username) == username.lower(),
                 self.model.deleted_at.is_(None)
             )
         )
@@ -17,7 +17,7 @@ class CRUDUser(CRUDBase[User, Any, Any]):
     async def get_by_email(self, db: AsyncSession, email: str) -> Optional[User]:
         result = await db.execute(
             select(self.model).where(
-                self.model.email == email,
+                func.lower(self.model.email) == email.lower(),
                 self.model.deleted_at.is_(None)
             )
         )
